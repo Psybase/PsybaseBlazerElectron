7f4f644c266da5b40c2630760a458e19763fa4b2
Psybase <support@psybase.co.za>
2021-04-06 10:28:47 +0200

    electronblazor

---
+++ PsybaseBlazerElectron/Shared/SampleBaseComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+
+namespace PsybaseBlazerElectron.Shared
+{
+    public class SampleBaseComponent : ComponentBase
+    {
+        [Inject]
+#pragma warning disable IDE1006 // Naming Styles
+        protected IJSRuntime jsRuntime { get; set; }
+#pragma warning restore IDE1006 // Naming Styles
+
+        [Inject]
+        protected SampleService Service { get; set; }
+
+       
+        protected override void OnAfterRender(bool FirstRender)
+        {
+            Service.Update(new NotifyProperties() { HideSpinner = true, RestricMouseEvents = true });
+        }
+    }
+}
---
+++ PsybaseBlazerElectron/Shared/SampleService.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Components;
+
+namespace PsybaseBlazerElectron.Shared
+{
+    public class SampleService
+    {
+        // Can be called from anywhere
+        public void Update(NotifyProperties prop)
+        {
+            if (Notify != null)
+            {
+                this.Prop = prop;
+                Notify.Invoke(prop);
+            }
+        }
+
+        public NotifyProperties Prop
+        {
+            get; set;
+        } = new NotifyProperties();
+
+        public CurrentSampleData Data { get; set; } = new CurrentSampleData();
+        public event Func<NotifyProperties, Task> Notify;
+    }
+
+    public class NotifyProperties
+    {
+        public bool HideSpinner { get; set; }
+
+        public bool RestricMouseEvents { get; set; }
+    }
+
+    public class CurrentSampleData
+    {
+        public CurrentSampleData(NavigationManager uriHelper)
+        {
+            this.UriHelper = uriHelper;
+        }
+
+        public CurrentSampleData()
+        {
+
+        }
+        public NavigationManager UriHelper { get; set; }
+        
+
+        
+
+        internal void Update(NavigationManager urlHelper)
+        {
+            this.UriHelper = urlHelper;
+            string updatedUrl = UriHelper.Uri.Replace(UriHelper.BaseUri, "").Trim();
+            if (updatedUrl.Contains('?'))
+            {
+                updatedUrl = updatedUrl.Substring(0, updatedUrl.IndexOf('?'));
+            }
+            if (updatedUrl != "")
+            {
+                if (updatedUrl.LastIndexOf("/") == updatedUrl.Length - 1)
+                {
+                    updatedUrl = updatedUrl.Substring(0, updatedUrl.LastIndexOf("/"));
+                }
+                updatedUrl = updatedUrl.Replace("//", "/");
+                string[] splittedUrl = updatedUrl.Split("/");
+                if (splittedUrl.Length >= 2)
+                {
+                    try
+                    {
+                        string CategoryName = splittedUrl[^2];
+                        string SampleName = splittedUrl[^1];
+                        CategoryName = CategoryName.Replace("-", "");
+
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+                if (splittedUrl.Length < 2 && splittedUrl.Length > 0)
+                {
+                    string CategoryName = splittedUrl[^1];
+                    // CategoryName = char.ToUpper(CategoryName[0]) + CategoryName.Substring(1);
+                    _ = CategoryName.Replace("-", "");
+
+                }
+            }
+        }
+    }
+}
